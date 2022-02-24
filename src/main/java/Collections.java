671b5dfcd9620a50e276650bd197f95f5507a454
MrEmmanuel <sejesoo@gmail.com>
2022-02-25 00:33:19 +0200

    Implemented all the requirements

---
+++ src/main/java/Collections.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Collections {
+
+     int count = 0;
+     HashSet<Integer> set = new HashSet<>();
+     Deque<Object> queue = new ArrayDeque<>();
+
+    public int maximumNumberUniqueIntegers(int subArraySize, int[] array) {
+        int intNumber = array.length;
+        for (int i = 0; i < intNumber; i++){
+            queue.add(array[i]);
+        }
+        for (int i = 0; i < intNumber - subArraySize; i++) {
+            Object[] temp = queue.toArray();
+            for (int j = 0; j < subArraySize; j++) {
+                set.add((int)temp[j]);
+            }
+            queue.remove();
+            if (set.size() > count) count = set.size();
+            set.clear();
+        }
+        return count;
+    }
+}
