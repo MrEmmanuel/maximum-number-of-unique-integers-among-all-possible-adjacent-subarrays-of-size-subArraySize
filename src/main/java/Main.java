f505c69b5922cd2f05806c81db8be8f2bcbb52d0
Oreneile Sejeso <sejesoo@gmail.com>
2022-03-04 12:36:20 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args){
         Collections max = new Collections();
 
-        System.out.println(max.maximumNumberUniqueIntegers(8,4, new int[]{2,2,2,2,2,2,2,2}));
+        System.out.println(max.maxUniqueIntegers(8,4, new int[]{2,2,2,2,2,2,2,2}));
     }
 }
