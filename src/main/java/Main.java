d27d9d392d0cccc9ecaaeeabebeb4c5d9daa0c66
Oreneile Sejeso <sejesoo@gmail.com>
2022-04-12 14:06:42 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args){
         Collections max = new Collections();
 
-        System.out.println(max.maxUniqueIntegers(8,4, new int[]{2,2,2,2,2,2,2,2}));
+        System.out.println(max.maxUniqueIntegers(10, 3, new ArrayDeque<>(Arrays.asList(7, 5, 5, 7, 5, 5, 7, 5, 5, 6))));
+
     }
 }
